97ad6af2d86c34dad5f93bbd68d946cf85f98e1b
Sandys Nunes <sandysnunes@gmail.com>
2020-05-13 00:21:05 -0400

    inserting fake todo

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/sandysnunes/todo-server/src/controllers"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("")
+
+	db, err := sqlx.Connect(
+		"postgres",
+		"postgres://postgres:postgres@localhost:5432/todo?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	http.HandleFunc("/", controllers.Create(db))
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
---
+++ src/controllers/todo-controller.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func Create(db *sqlx.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		err := db.QueryRow("INSERT INTO todo(title, description) VALUES ($1, $2)", "titulo", "descricao")
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("Internal error"))
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+	}
+}
